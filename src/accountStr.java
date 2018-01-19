713deec5ce7365dce3ec1af176315180fcfe0671
Elaine <chengc32@ziroom.com>
2018-01-19 09:57:44 +0800

    add stringnumer

--- src/accountStr.java
+++ src/accountStr.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
  * Created by chengc32 on 2018/1/18.
  */
 public class accountStr {
+    // 第一种方法
     public static Pattern patternNumber = Pattern.compile("(.)\\1*");
     public static String accountNum(String value) {
 
@@ -32,15 +33,58 @@ public class accountStr {
             }
         }
 
-        System.out.println(result.size());
-        System.out.println(result.toString());
+        System.out.println("第一种方法"+result.size());
+        System.out.println("第一种方法"+result.toString());
         return result.toString();
 
+    }
+
+    // 第二种方法
+    public static void getChars(char[] chars){
+        int count = 0;
+        int temp = 0;
+        List<String> result = new ArrayList<String>();
+
+        for(int i = 0; i<chars.length;i++){
+            for(int j = i; j< chars.length;j++){
+
+                if(i == chars.length -1 && chars[i-1] != chars[i]){
+                    result.add(chars[i] + "");
+                }else if(i == chars.length -1 && chars[i-1] == chars[i]){
+                    result.add(chars[i] + "");
+                    result.add((chars.length-1)- temp + "");
+                }
+                else{
+                    if(chars[i] != chars[j]){
+                        result.add(chars[i] + "");
+                        count = j - i;
+                        if (count < 10) {
+                            if(count != 1) {
+                                result.add(count + "");
+                            }
+                        } else if(count >= 10){
+                            char[] size = String.valueOf(count).toCharArray();
+                            for (int n = 0; n < size.length; n++) {
+                                result.add(String.valueOf(size[n]));
+                            }
+                        }
+
+                        temp = j - 1;
+                        i = temp;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println("第二种方法"+result.size());
+        System.out.println("第二种方法"+result.toString());
     }
 
     public static void main(String[] str) {
         char[] chars = {'a', 'a', 'b', 'b', 'b','b','b','b','b', 'b', 'b','b','b','b', 'c', 'a', 'a', 'b'};
         String s = String.valueOf(chars);
         accountNum(s);
+        getChars(chars);
     }
 }
